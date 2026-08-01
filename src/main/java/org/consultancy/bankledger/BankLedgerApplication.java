cda86bb04a80888bf13b743136bafc3e5ecfb825
Anjila Sipai <anjilasipai@macbookpro.mynetworksettings.com>
2026-08-01 12:27:10 -0400

    Initial commit: health check connected to RDS

---
+++ src/main/java/org/consultancy/bankledger/BankLedgerApplication.java
@@ -0,0 +1,13 @@
+package org.consultancy.bankledger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankLedgerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankLedgerApplication.class, args);
+    }
+
+}
---
+++ src/main/java/org/consultancy/bankledger/controller/HealthController.java
@@ -0,0 +1,13 @@
+package org.consultancy.bankledger.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public String health() {
+        return "Bank ledger service is up";
+    }
+}
